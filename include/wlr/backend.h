f84099fede577a5b0f2ba56ccdf7e1b3c784b952
Drew DeVault <sir@cmpwn.com>
2017-10-29 12:12:37 -0400

    Type summary and type reference placeholders

--- include/wlr/backend.h
+++ include/wlr/backend.h
@@ -11,16 +11,30 @@ struct wlr_backend {
 	const struct wlr_backend_impl *impl;
 
 	struct {
+		/** Raised when a wlr_input_device is added. */
 		struct wl_signal input_add;
+		/** Raised when a wlr_input_device is removed. */
 		struct wl_signal input_remove;
+		/** Raised when a wlr_output is added. */
 		struct wl_signal output_add;
+		/** Raised when a wlr_output is removed. */
 		struct wl_signal output_remove;
 	} events;
 };
 
+/**
+ * Examines the runtime environment and creates the most suitable backend.
+ */
 struct wlr_backend *wlr_backend_autocreate(struct wl_display *display);
+
 bool wlr_backend_start(struct wlr_backend *backend);
+
 void wlr_backend_destroy(struct wlr_backend *backend);
+
+/**
+ * Returns the wlr_egl object for this backend, if applicable, or NULL
+ * otherwise.
+ */
 struct wlr_egl *wlr_backend_get_egl(struct wlr_backend *backend);
 
 #endif
